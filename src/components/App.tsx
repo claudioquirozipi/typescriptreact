6c0d336f78b95f50dc9bf5a55a74a8a2e764bcca
Claudio Quiroz <claudioquirozipi@gmail.com>
2019-07-25 00:24:15 -0500

    Segundo commit

---
+++ src/components/App.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import styled from 'styled-components';
+
+const H2 = styled.h2`
+    color: red;
+`;
+
+function App() {
+    return(
+        <>
+        <h1>Hola mundo</h1>
+        <H2>Hola dos</H2>
+        </>
+    );
+}
+export default App;
